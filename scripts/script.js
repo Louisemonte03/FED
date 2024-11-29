4be69ba373fca0d757872135372c2bda66ca8c6e
Louisemonte03 <157892261+Louisemonte03@users.noreply.github.com>
2024-11-29 15:31:54 +0100

    hjk

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,9 @@
-// JavaScript Document
-console.log("hi");
+const menuButton = document.getElementById("menuButton");
+const deNav = document.querySelector("header > nav");
+
+menuButton.onclick = toggleMenu;
+
+function toggleMenu() {
+    deNav.classList.toggle("open");
+}
+

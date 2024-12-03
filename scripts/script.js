87c2c0dd025d5d5aedec17bd1e79f730670c8096
Louisemonte03 <157892261+Louisemonte03@users.noreply.github.com>
2024-12-03 14:03:03 +0100

    eeeee

--- scripts/script.js
+++
@@ -1,9 +0,0 @@
-const menuButton = document.getElementById("menuButton");
-const deNav = document.querySelector("header > nav");
-
-menuButton.onclick = toggleMenu;
-
-function toggleMenu() {
-    deNav.classList.toggle("open");
-}
-
